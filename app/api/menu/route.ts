1773d2574f34a0cb17ba88307c53bc415b5cb2e7
Maicolalvarez-debug <maicolalvarez123.z@gmail.com>
2025-08-17 01:25:08 -0500

    Add files via upload

--- app/api/menu/route.ts
+++ app/api/menu/route.ts
@@ -1,5 +1,8 @@
 export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
 import { prisma } from "@/lib/prisma";
+
 export async function GET() {
   const categories = await prisma.category.findMany({ orderBy: { name: 'asc' } });
   const products = await prisma.product.findMany({
