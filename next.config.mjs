e4103568f43b6a91c5c2a86ba94d9f5033167a89
Maicolalvarez-debug <maicolalvarez123.z@gmail.com>
2025-08-18 08:49:49 -0500

    e

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Si tenías experimental.serverActions, elimínalo (en Next 14+ ya vienen por defecto)
-  // experimental: { serverActions: true },
-};
-
-export default nextConfig;
