a4109e8b1a2b8e14ec687f47b602c587045202e1
Maicolalvarez-debug <maicolalvarez123.z@gmail.com>
2025-08-21 13:48:38 -0500

    Add files via upload

--- app/api/promo/route.ts
+++ app/api/promo/route.ts
@@ -1,10 +1,11 @@
+export const runtime = 'nodejs';
 export const dynamic = "force-dynamic";
 
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
-export async function GET() {
+export async function GET(){ try:
   const now = new Date();
-  const promo = await prisma.dailyPromo.findFirst({ select: { id: true, active: true, startDate: true, endDate: true, title: true, description: true, image: true, imageUrl: true, price: true, cost: true },
+  const promo = await prisma.dailyPromo.findFirst({ select: { id: true, active: true, startDate: true, endDate: true, title: true, description: true, image: true, imageUrl: true, price: true, cost: true  } catch(err:any){ return NextResponse.json(null); } },
     where: { active: true, OR: [
       { startDate: null, endDate: null },
       { startDate: { lte: now }, endDate: null },
