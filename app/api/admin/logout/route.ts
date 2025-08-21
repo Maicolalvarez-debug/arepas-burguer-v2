63c5fefb7f49742807e0fee88418477fe1476cfa
Maicolalvarez-debug <maicolalvarez123.z@gmail.com>
2025-08-20 23:50:05 -0500

    gh

--- app/api/admin/logout/route.ts
+++
@@ -1,8 +0,0 @@
-
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const res = NextResponse.redirect(new URL('/admin-login', process.env.BASE_URL || 'http://localhost:3000'));
-  res.cookies.set('ab_admin', '', { httpOnly: true, path: '/', maxAge: 0 });
-  return res;
-}
